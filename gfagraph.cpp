3f0972fbec4f7db7fe9090156a91d08348885ee9
emapuljak <puljak-ema96@hotmail.com>
2020-01-12 18:47:05 +0100

    Update gfagraph.cpp

--- gfagraph.cpp
+++ gfagraph.cpp
@@ -1,10 +1,3 @@
-//
-//  gfagraph.cpp
-//  BioinformaticsProject
-//
-//  Created by Ema Puljak on 05/12/2019.
-//  Copyright © 2019 Ema Puljak. All rights reserved.
-//
 #include<assert.h>
 #include <stdio.h>
 #include <fstream>
@@ -44,68 +37,6 @@ nodes(),
 edges(),
 edgeOverlap(-1)
 {
-}
-
-
-GfaGraph GfaGraph::GetSubgraph(const std::unordered_set<int>& ids) const
-{
-    GfaGraph result;
-    result.edgeOverlap = edgeOverlap;
-    for (auto node : ids)
-    {
-        if (nodes.count(node) == 0) continue;
-        result.nodes[node] = nodes.at(node);
-        NodePos end {node, true};
-        if (edges.count(end) == 1)
-        {
-            for (auto target : edges.at(end))
-            {
-                if (ids.count(target.id) == 0) continue;
-                result.edges[end].push_back(target);
-            }
-        }
-        NodePos start {node, false};
-        if (edges.count(start) == 1)
-        {
-            for (auto target : edges.at(start))
-            {
-                if (ids.count(target.id) == 0) continue;
-                result.edges[start].push_back(target);
-            }
-        }
-    }
-    return result;
-}
-
-
-GfaGraph GfaGraph::GetSubgraph(const std::unordered_set<int>& nodeids, const std::unordered_set<std::pair<NodePos, NodePos>>& selectedEdges) const
-{
-    GfaGraph result;
-    result.edgeOverlap = edgeOverlap;
-    for (auto node : nodeids)
-    {
-        if (nodes.count(node) == 0) continue;
-        result.nodes[node] = nodes.at(node);
-        NodePos end {node, true};
-        if (edges.count(end) == 1)
-        {
-            for (auto target : edges.at(end))
-            {
-                if (nodeids.count(target.id) == 0) continue;
-                if (selectedEdges.count({end, target}) == 1 || selectedEdges.count({target, end}) == 1) result.edges[end].push_back(target);
-            }
-        }
-        NodePos start {node, false};
-        if (edges.count(start) == 1)
-        {
-            for (auto target : edges.at(start))
-            {
-                if (nodeids.count(target.id) == 0) continue;
-                if (selectedEdges.count({start, target}) == 1 || selectedEdges.count({target, start}) == 1) result.edges[start].push_back(target);
-            }
-        }
-    }
-    return result;
 }
 
 GfaGraph GfaGraph::LoadFromFile(std::string filename)
